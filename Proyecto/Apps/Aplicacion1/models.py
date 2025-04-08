a7b5da6fbfe0b874dff3c14f1916facb5255258d
Felipe_Espinel <espinelbotina@gmail.com>
2025-04-08 10:15:38 -0500

    Actualización IX v22

--- Proyecto/Apps/Aplicacion1/models.py
+++ Proyecto/Apps/Aplicacion1/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from datetime import date, timedelta
+
 from django.core.exceptions import ValidationError
 
 class Rol(models.Model):
